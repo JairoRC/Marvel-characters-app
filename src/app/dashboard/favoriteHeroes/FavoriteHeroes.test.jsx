ac7c3c215c2a1f375ad3aac96d8e76b12a0b43f7
JairoRC <jairorguezdeveloper@gmail.com>
2024-06-19 12:08:05 +0200

    comentarios eliminados

--- src/app/dashboard/favoriteHeroes/FavoriteHeroes.test.jsx
+++ src/app/dashboard/favoriteHeroes/FavoriteHeroes.test.jsx
@@ -5,12 +5,10 @@ import { useHeroes } from "../../../context/HeroesContext";
 import { SearchBar } from "../../../components/SearchBar/SearchBar";
 import { HeroeGrid } from "../../../heroes/components/HeroeGrid";
 
-// Mock del contexto
 jest.mock("@/context/HeroesContext", () => ({
   useHeroes: jest.fn(),
 }));
 
-// Mock de componentes
 jest.mock("@/components/SearchBar/SearchBar", () => ({
   SearchBar: ({ onSearch, displayedHeroes }) => (
     <div data-testid="search-bar">
@@ -36,7 +34,6 @@ jest.mock("@/heroes", () => ({
   ),
 }));
 
-// Datos de prueba
 const mockFavorites = [
   {
     id: 1,
@@ -59,25 +56,20 @@ beforeEach(() => {
 test("renderiza correctamente los héroes favoritos y filtra por término de búsqueda", () => {
   render(<FavoritesHeroesPage />);
 
-  // Verificar que el componente SearchBar se renderiza correctamente
   expect(screen.getByTestId("search-bar")).toBeInTheDocument();
 
-  // Verificar que el HeroeGrid se renderiza con los héroes favoritos
   expect(screen.getByTestId("heroe-grid")).toBeInTheDocument();
   expect(screen.getAllByTestId("heroe-card").length).toBe(mockFavorites.length);
   expect(screen.getByText("Spider-Man")).toBeInTheDocument();
   expect(screen.getByText("Iron Man")).toBeInTheDocument();
 
-  // Filtrar héroes por término de búsqueda
   fireEvent.change(screen.getByTestId("search-input"), {
     target: { value: "Spider" },
   });
 
-  // Verificar que HeroeGrid muestra solo los héroes filtrados
   expect(screen.getAllByTestId("heroe-card").length).toBe(1);
   expect(screen.getByText("Spider-Man")).toBeInTheDocument();
   expect(screen.queryByText("Iron Man")).not.toBeInTheDocument();
 
-  // Verificar que el número de resultados se actualiza
   expect(screen.getByTestId("results-count").textContent).toBe("1 RESULTS");
 });
